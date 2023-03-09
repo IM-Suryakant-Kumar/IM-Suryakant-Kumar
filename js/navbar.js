8e35a57cc15b9709ed3da3f124161424b0057226
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-03-09 22:28:03 +0530

    docs: Updated navbar

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,5 @@ const menuBar = document.querySelector(".menu-bar");
 menuBar.addEventListener("click", () => {
   const showMenu = document.querySelector(".menu");
   showMenu.classList.toggle("show-menu");
-
-  menuBar.classList.toggle("fa-xmark");
-  menuBar.classList.toggle("fa-bars");
+  menuBar.classList.toggle("active");
 });
--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const linkPrimary = document.querySelector(".primary-link");
-const linkSecondary = document.querySelector(".secondary-link");
+// const linkPrimary = document.querySelectorAll(".primary-link");
+// const linkSecondary = document.querySelectorAll(".secondary-link");
 
-// Primary Link
+// // Primary Link
 
-linkPrimary.addEventListener("mousedown", () => {
-    document.querySelector('.primary-link').classList.add('link-clicked');
-})
+// linkPrimary.addEventListener("mousedown", () => {
+//     document.querySelector('.primary-link').classList.add('link-clicked');
+// })
 
-linkPrimary.addEventListener("mouseup", () => {
-    document.querySelector('.primary-link').classList.remove('link-clicked');
-})
+// linkPrimary.addEventListener("mouseup", () => {
+//     document.querySelector('.primary-link').classList.remove('link-clicked');
+// })
 
-// Secondary Link
+// // Secondary Link
 
-linkSecondary.addEventListener("mousedown", () => {
-    document.querySelector('.secondary-link').classList.add('link-clicked');
-})
+// linkSecondary.addEventListener("mousedown", () => {
+//     document.querySelector('.secondary-link').classList.add('link-clicked');
+// })
 
-linkSecondary.addEventListener("mouseup", () => {
-    document.querySelector('.secondary-link').classList.remove('link-clicked');
-})
+// linkSecondary.addEventListener("mouseup", () => {
+//     document.querySelector('.secondary-link').classList.remove('link-clicked');
+// })
