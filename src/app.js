be65b5ca1f4ba612bdf722e84cc622df27396fbf
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-24 10:23:36 +0530

    refactor: organize menuBar func

--- src/app.js
+++ src/app.js
@@ -1,9 +1,2 @@
-import "./scripts/hero.js"
-
-const menuBar = document.querySelector(".menu-bar");
-
-menuBar.addEventListener("click", () => {
-	const menu = document.querySelector(".menu");
-	menuBar.classList.toggle("active");
-	menu.classList.toggle("active");
-});
+import "./scripts/hero.js";
+import "./scripts/menu-bar.js";
