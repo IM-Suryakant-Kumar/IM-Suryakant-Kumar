789d84e3e1f7ac0445b41fc888c23216d18e16a1
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-25 15:53:22 +0530

    feat: realeased portfolio v2

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-const menuBar = document.querySelector(".menu-bar");
-
-menuBar.addEventListener("click", () => {
-  const showMenu = document.querySelector(".menu");
-  showMenu.classList.toggle("show-menu");
-  menuBar.classList.toggle("active");
-});
