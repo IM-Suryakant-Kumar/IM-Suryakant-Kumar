bce355fa12116dc51f25871ad92c313865630efb
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-02-17 13:34:34 +0530

    feat: Added navbar

--- new/src/script.js
+++ new/src/script.js
@@ -1,13 +1,26 @@
 const menuBar = document.querySelector(".menu-bar");
+const menuList = document.querySelector(".menu-list");
 
 // menu-bar start
-menuBar.innerHTML =  `
+menuBar.innerHTML = `
   <div class="bar top"></div>
   <div class="bar middle"></div>
   <div class="bar bottom"></div>
-`
+`;
 
 menuBar.addEventListener("click", (e) => {
-  menuBar.classList.toggle("active");
-})
+	menuBar.classList.toggle("active");
+	menuList.classList.toggle("active");
+});
 // menu-bar end
+
+// menu-list start
+menuList.addEventListener("click", (e) => {
+	menuBar.classList.toggle("active");
+	menuList.classList.toggle("active");
+
+  // remove any active class from previous menu link and add to clicked link
+  document.querySelector("li > a.active").classList.remove("active");
+  e.target.classList.add("active");
+});
+// menu-list end
