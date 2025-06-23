4fb095005857afedb94095b45292ade2fa0d45c8
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-06-23 15:06:31 +0530

    style: Updated logo color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 	return (
 		<header className="fixed top-0 left-0 right-0 z-50 bg-primary text-secondary border-b-2 border-gray-200">
 			<nav className="w-full max-w-7xl mx-auto h-15 px-2 flex justify-between items-center">
-				<h1 className="text-2xl md:text-3xl font-cinzel font-bold">
+				<h1 className="text-orange-500 text-2xl md:text-3xl font-cinzel font-bold">
 					Suryakant
 				</h1>
 				<Menubar />
