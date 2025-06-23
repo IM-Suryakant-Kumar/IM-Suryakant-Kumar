2cc80cf44f5c7fc7d74ebf2b09085e9e64a958af
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2025-06-23 15:28:36 +0530

    style: Updated Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-	return <footer className="p-4 pt-6 border-t-2 border-gray-200 text-gray-600 leading-relaxed text-sm text-center">
+	return <footer className="p-4 pt-6 border-t-2 border-gray-200 text-gray-600 leading-relaxed text-sm text-center select-none">
     <p>© 2025, all rights reserved</p>
     <p>Designed & Built by <span className="text-orange-400 font-cinzel font-semibold">Suryakant Kumar</span></p>
   </footer>;
